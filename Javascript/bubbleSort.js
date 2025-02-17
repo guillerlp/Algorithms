52bb296715a189ad99d78ca8dc373ee127545672
Guillermo López Pérez <glopez@visiotechsecurity.com>
2025-02-17 10:02:02 +0100

    Add bubble sort

---
+++ Javascript/bubbleSort.js
@@ -0,0 +1,23 @@
+function bubbleSort(array) {
+    // Only change code below this line
+    for(let i = 0; i < array.length ; i++){
+      for(let j = 0; j < array.length - 1 - i; j++){
+        if(array[j] > array[j + 1]){
+            let temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }
+      }
+    }
+
+    return array;
+    // Only change code above this line
+  }
+
+console.log(bubbleSort([5, 3, 6, 1, 8, 7, 2, 4]));
+
+
+// Bubble Sort is a sorting algorithm which sorts or bubbles the largest number as last element at the end of each pass.
+// We compare each element to the one ahead of it, if the element before is smaller, we swap their places.
+// Bubble Sort’s time complexity is O(n2).
+// It’s a stable algorithm.
